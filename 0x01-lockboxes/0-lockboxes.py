2b777af29d8c89d579707bc885744119432a45f6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-07-21 14:09:20 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""canUnlockAll function."""
+"""Function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
 
 
 def canUnlockAll(boxes):
-    """Method that determines if all the boxes can be opened """
-    if not boxes:
-        return False
-    
-    opened = set()
-    stack = [0]
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
 
-    while stack:
-        current_box = stack.pop()
-        if current_box not in opened:
-            opened.add(current_box)
-            for key in boxes[current_box]:
-                if key not in opened and key < len(boxes):
-                    stack.append(key)
-    
-    return len(opened) == len(boxes)
-
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
